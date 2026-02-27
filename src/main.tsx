2829e990bbcb5b7276478e1e57d7e7bcb3a441a1
Lokesh-2403 <adusumalli3818@gmail.com>
2026-02-27 17:32:48 +0530

    loading page

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-if (!sessionStorage.getItem('hasSeenLoader')) {
+const params = new URLSearchParams(window.location.search);
+
+if (!params.has('loaded')) {
   window.location.replace('/loading.html');
 } else {
-  sessionStorage.removeItem('hasSeenLoader');
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <App />
