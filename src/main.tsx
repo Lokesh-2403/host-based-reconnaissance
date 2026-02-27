f32e3eee3571bc30f365c7d1e7bdfdb9cccc65e0
Lokesh-2403 <adusumalli3818@gmail.com>
2026-02-27 18:13:46 +0530

    final update

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-const params = new URLSearchParams(window.location.search);
-
-if (!params.has('loaded')) {
-  window.location.replace('/loading.html');
-} else {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
