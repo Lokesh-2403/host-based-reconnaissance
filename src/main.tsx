239182932ba78dae1f844da6e04cd171996f971e
Lokesh-2403 <adusumalli3818@gmail.com>
2026-02-27 17:26:22 +0530

    loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-// Show loading page on first visit
-if (!sessionStorage.getItem('loaded')) {
-  sessionStorage.setItem('loaded', 'true')
-  window.location.replace('/loading.html')
-}
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (!sessionStorage.getItem('hasSeenLoader')) {
+  window.location.replace('/loading.html');
+} else {
+  sessionStorage.removeItem('hasSeenLoader');
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
